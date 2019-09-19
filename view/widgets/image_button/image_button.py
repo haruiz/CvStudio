450e800207a75e922680439ee00fff8f3c34733b
Henry Ruiz <henryruiz22@gmail.com>
2019-09-19 15:11:17 -0500

    refresh dataset option added

--- view/widgets/image_button/image_button.py
+++ view/widgets/image_button/image_button.py
@@ -1,3 +1,5 @@
+import math
+
 from PyQt5 import QtCore,QtGui
 from PyQt5.QtCore import QSize,QObject,pyqtSignal
 from PyQt5.QtGui import QIcon
@@ -6,20 +8,31 @@ from PyQt5.QtWidgets import QPushButton,QGraphicsDropShadowEffect
 
 class ImageButton(QPushButton, QObject):
     doubleClicked = pyqtSignal(QtGui.QMouseEvent)
-    def __init__(self,icon: QIcon = QIcon(), size: QSize=QSize(64,64),  parent=None, with_shadow= False):
+    def __init__(self,icon: QIcon = QIcon(), size: QSize=QSize(64,64),  parent=None, with_shadow= True):
         super(ImageButton, self).__init__(parent=parent, icon=icon)
         self.setContentsMargins(10,10,10,20)
         self.setCursor(QtCore.Qt.PointingHandCursor)
-        self.setIconSize(size)
+        self._size = size
+        self._effect = self.graphicsEffect()
+        self.setIconSize(self._size)
         self.setObjectName("image_button")
-        shadow=QGraphicsDropShadowEffect(self)
-        shadow.setBlurRadius(12)
-        shadow.setColor(QtGui.QColor(76,35,45).lighter())
         self.setStyleSheet('QPushButton{border: 0px solid;}')
         # shadow.setColor(QtGui.QColor(99, 255, 255))
-        shadow.setOffset(2)
-        if with_shadow:
-            self.setGraphicsEffect(shadow)
+
+    def enterEvent(self, evt: QtCore.QEvent) -> None:
+        #self.setIconSize(QSize(math.floor(self._size.width() * 1.2),math.floor(self._size.height() * 1.2)))
+        shadow=QGraphicsDropShadowEffect(self)
+        shadow.setBlurRadius(8)
+        #shadow.setColor(QtGui.QColor(76,35,45).lighter())
+        shadow.setColor(QtGui.QColor(76,35,45).lighter())
+        shadow.setOffset(4)
+        self.setGraphicsEffect(shadow)
+        super(ImageButton, self).enterEvent(evt)
+
+    def leaveEvent(self, evt: QtCore.QEvent) -> None:
+        self.setGraphicsEffect(self._effect)
+        self.setIconSize(self._size)
+        super(ImageButton, self).leaveEvent(evt)
 
     def mouseDoubleClickEvent(self, evt: QtGui.QMouseEvent) -> None:
         self.doubleClicked.emit(evt)
