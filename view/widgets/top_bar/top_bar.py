bf8b4697157f832067dec1860263a976d88c0867
Henry Ruiz <henryruiz22@gmail.com>
2019-12-20 13:24:25 -0600

    pyface error

--- view/widgets/top_bar/top_bar.py
+++ view/widgets/top_bar/top_bar.py
@@ -1,7 +1,7 @@
-from PyQt5 import QtCore
+from PyQt5 import QtCore,QtGui
 from PyQt5.QtCore import QSize
 from PyQt5.QtWidgets import QFrame,QVBoxLayout,QHBoxLayout,QPushButton
-from pyface.qt import QtGui
+
 
 from util import GUIUtilities
 
